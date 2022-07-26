1076332b96925ba391cc0903953986eca05622ad
Simon Rosen <simonrosen173@gmail.com>
2022-07-26 15:29:56 +0200

    Added stuff for pip install [this project]

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='multi-agent-grid-world',
+    version='0.1',
+    packages=find_packages(),
+    url='',
+    license='MIT',
+    author='Simon Rosen',
+    author_email='simonrosen173@gmail.com',
+    description='Multi-agent grid world gym environment '
+)
